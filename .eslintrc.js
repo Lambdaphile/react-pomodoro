880dc9a94f2f6b52af67686e49ba18bf260bfed0
genesis-algorithms <genesisalgorithms@gmail.com>
2019-09-05 22:45:44 +0300

    chore: Tweak for handling React...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
+  "parser": "babel-eslint",
   env: {
     browser: true,
     es6: true,
   },
   extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
     'airbnb',
   ],
   globals: {
