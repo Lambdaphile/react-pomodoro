bbb164291892f904faa6cef135a7e80ed8658dce
genesis-algorithms <genesisalgorithms@gmail.com>
2019-09-09 20:27:38 +0300

    Config eslint to help with React Hooks...

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,10 @@ module.exports = {
   },
   plugins: [
     'react',
+    'react-hooks'
   ],
   rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   },
 };
