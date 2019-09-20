850c6272aff906036a7960f8480655cf1199de9a
genesis-algorithms <genesisalgorithms@gmail.com>
2019-09-21 00:23:35 +0300

    Rename folder utils to granim...

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './utils/granim.css';
+import './granim/granim.css';
 import 'semantic-ui-css/semantic.min.css';
 
-import './utils/granim';
+import './granim/granim';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
