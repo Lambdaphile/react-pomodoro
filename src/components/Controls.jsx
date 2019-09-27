f821a01bf15f8502b851704bb5d40d34bfdeefe4
genesis-algorithms <genesisalgorithms@gmail.com>
2019-09-27 22:02:52 +0300

    style: Switch using tabs(2) instead of spaces
    VSCode tabs icon too good to resist... :D
    Also think it's more practical to use tabs (easier when deleting...)
    And modern editors/ide's handle difference between tabs/spaces well enough.

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -2,99 +2,93 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Grid, Header, Icon, Modal } from 'semantic-ui-react';
 
-const Controls = ({
-  isDisabled,
-  onSkip,
-  onStart,
-  onStop,
-  textContext,
-}) => {
-  const [modalState, setModalState] = useState(false);
+const Controls = ({ isDisabled, onSkip, onStart, onStop, textContext }) => {
+	const [modalState, setModalState] = useState(false);
 
-  const handleOpen = () => {
-    setModalState(true);
-  };
+	const handleOpen = () => {
+		setModalState(true);
+	};
 
-  const handleYes = () => {
-    onStop();
-    setModalState(false);
-  };
-  const handleNo = () => setModalState(false);
+	const handleYes = () => {
+		onStop();
+		setModalState(false);
+	};
+	const handleNo = () => setModalState(false);
 
-  return (
-    <>
-      <Grid centered stackable>
-        <Grid.Column width="3">
-          <Button
-            circular
-            color="grey"
-            fluid
-            inverted
-            onClick={onStart}
-            size="huge"
-          >
-            {textContext}
-          </Button>
-        </Grid.Column>
-        <Grid.Column width="3">
-          <Modal
-            basic
-            open={modalState}
-            size="small"
-            trigger={
-              <Button
-                circular
-                color="grey"
-                disabled={isDisabled}
-                fluid
-                inverted
-                onClick={handleOpen}
-                size="huge"
-              >
-                Stop
-              </Button>
-            }
-          >
-            <Header content="Stop Pomodoro" />
-            <Modal.Content>
-              <p>Are you sure you want to reset Pomodoro?</p>
-            </Modal.Content>
-            <Modal.Actions>
-              <Button basic color="red" inverted onClick={handleNo}>
-                <Icon name="remove" />
-                No
-              </Button>
-              <Button color="green" inverted onClick={handleYes}>
-                <Icon name="checkmark" />
-                Yes
-              </Button>
-            </Modal.Actions>
-          </Modal>
-        </Grid.Column>
-        <Grid.Column width="3">
-          <Button
-            circular
-            color="grey"
-            disabled={isDisabled}
-            fluid
-            inverted
-            onClick={onSkip}
-            size="huge"
-          >
-            Skip
-          </Button>
-        </Grid.Column>
-      </Grid>
-    </>
-  );
+	return (
+		<>
+			<Grid centered stackable>
+				<Grid.Column width="3">
+					<Button
+						circular
+						color="grey"
+						fluid
+						inverted
+						onClick={onStart}
+						size="huge"
+					>
+						{textContext}
+					</Button>
+				</Grid.Column>
+				<Grid.Column width="3">
+					<Modal
+						basic
+						open={modalState}
+						size="small"
+						trigger={
+							<Button
+								circular
+								color="grey"
+								disabled={isDisabled}
+								fluid
+								inverted
+								onClick={handleOpen}
+								size="huge"
+							>
+								Stop
+							</Button>
+						}
+					>
+						<Header content="Stop Pomodoro" />
+						<Modal.Content>
+							<p>Are you sure you want to reset Pomodoro?</p>
+						</Modal.Content>
+						<Modal.Actions>
+							<Button basic color="red" inverted onClick={handleNo}>
+								<Icon name="remove" />
+								No
+							</Button>
+							<Button color="green" inverted onClick={handleYes}>
+								<Icon name="checkmark" />
+								Yes
+							</Button>
+						</Modal.Actions>
+					</Modal>
+				</Grid.Column>
+				<Grid.Column width="3">
+					<Button
+						circular
+						color="grey"
+						disabled={isDisabled}
+						fluid
+						inverted
+						onClick={onSkip}
+						size="huge"
+					>
+						Skip
+					</Button>
+				</Grid.Column>
+			</Grid>
+		</>
+	);
 };
 
 Controls.propTypes = {
-  isDisabled: PropTypes.bool.isRequired,
-  onSkip: PropTypes.func.isRequired,
-  onStart: PropTypes.func.isRequired,
-  onStop: PropTypes.func.isRequired,
-  textContext: PropTypes.string.isRequired,
+	isDisabled: PropTypes.bool.isRequired,
+	onSkip: PropTypes.func.isRequired,
+	onStart: PropTypes.func.isRequired,
+	onStop: PropTypes.func.isRequired,
+	textContext: PropTypes.string.isRequired,
 };
 
 export default Controls;
--- src/hooks/useInterval.jsx
+++ src/hooks/useInterval.jsx
@@ -1,29 +1,25 @@
 import { useEffect, useRef } from 'react';
 
 const useInterval = (callback, delay, timerState) => {
-  const savedCallback = useRef();
+	const savedCallback = useRef();
 
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
+	// Remember the latest callback.
+	useEffect(() => {
+		savedCallback.current = callback;
+	}, [callback]);
 
-  // Set up the interval.
-  useEffect(() => {
-    const tick = () => savedCallback.current();
+	// Set up the interval.
+	useEffect(() => {
+		const tick = () => savedCallback.current();
 
-    let id;
-    if (
-      delay !== null &&
-      timerState !== 'stopped' &&
-      timerState !== 'paused'
-    ) {
-      id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
+		let id;
+		if (delay !== null && timerState !== 'stopped' && timerState !== 'paused') {
+			id = setInterval(tick, delay);
+			return () => clearInterval(id);
+		}
 
-    return () => clearInterval(id);
-  }, [delay, timerState]);
+		return () => clearInterval(id);
+	}, [delay, timerState]);
 };
 
 export default useInterval;
