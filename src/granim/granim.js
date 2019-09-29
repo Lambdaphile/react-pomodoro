50f6c988a0afcca3afbc653ba3d00a0310459832
genesis-algorithms <genesisalgorithms@gmail.com>
2019-09-29 16:03:45 +0300

    Change granimjs background image and gradient order...

--- src/granim/granim.js
+++ src/granim/granim.js
@@ -12,12 +12,12 @@ const granimInstance = new Granim({
   states: {
     'default-state': {
       gradients: [
+        ['#29323c', '#485563'],
+        ['#FF6B6B', '#556270'],
         ['#80d3fe', '#7ea0c4'],
         ['#f0ab51', '#eceba3'],
-        ['#FF6B6B', '#556270'],
-        ['#29323c', '#485563'],
       ],
-      transitionSpeed: 5000,
+      transitionSpeed: 6000,
     },
   },
 });
